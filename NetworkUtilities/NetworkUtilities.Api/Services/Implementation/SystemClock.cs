42d5410976a7cbcbcc277294d10d7d03264ba581
lmjanssens <luukmjanssens@gmail.com>
2026-01-29 23:20:33 +0100

    Readded NetworkUtilities project/solution

---
+++ NetworkUtilities/NetworkUtilities.Api/Services/Implementation/SystemClock.cs
@@ -0,0 +1,14 @@
+﻿using NetworkUtilities.Api.Services.Interfaces;
+
+namespace NetworkUtilities.Api.Services.Implementation
+{
+    public class SystemClock : ISystemClock
+    {
+        public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+
+        public DateTimeOffset LocalNow => DateTimeOffset.Now;
+
+        public string LocalTimeZoneId => TimeZoneInfo.Local.Id;
+
+    }
+}
---
+++ NetworkUtilities/NetworkUtilities.Api/Services/Interfaces/ISystemClock.cs
@@ -0,0 +1,10 @@
+﻿namespace NetworkUtilities.Api.Services.Interfaces
+{
+    public interface ISystemClock
+    {
+        DateTimeOffset UtcNow { get; }
+        DateTimeOffset LocalNow { get; }
+        string LocalTimeZoneId { get; }
+
+    }
+}
